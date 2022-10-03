692b423f5544e7c9f595a9103ef32d7d12cea420
Alyssa Wendt <alyssa95wendt@gmail.com>
2022-10-03 10:54:16 -0400

    init veiws is set up and models is migrated, working on update.

--- finch_project/settings.py
+++ finch_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'finch_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'finch',
     }
 }
@@ -121,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.base import TemplateView
+from .models import Finch
 
-# Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
+
+class FinchList(TemplateView):
+    template_name = "finch_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        # If a query exists we will filter by name 
+        print(name)
+        if name != None:
+            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+            context["finch"] = Finch.objects.filter(name__icontains=name)
+            context["stuff_at_top"] = f"Searching through Finch list for {name}"
+        else:
+            context["finch"] = Finch.objects.all()
+            context["stuff_at_top"] = "Cool Birds"
+        return context
